25a7a300dfb6c5c8e64d4ac663479744d7e41785
trco1011 <trco1011@eBusiness-PC.localdomain>
2013-04-24 14:01:57 +0200

    MVN Commit

--- Klinikum/src/de/klinikum/server/Connect.java
+++ Klinikum/src/de/klinikum/server/Connect.java
@@ -1,5 +1,32 @@
 package de.klinikum.server;
 
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.http.HTTPRepository;
+
 public class Connect {
+
+	public class Connection {
+		String sesameServer = "http://localhost:8080/openrdf-sesame";
+		String repositoryID = "TestNative";
+		
+		
+			
+		public Repository GetRepository()
+		{
+			Repository myRepository = new HTTPRepository(sesameServer, repositoryID);
+			try {
+				myRepository.initialize();
+			} catch (RepositoryException e) {
+				
+				e.printStackTrace();
+			}
+
+			return myRepository;
+		}
+		
+	}
+
+	
 	
 }
