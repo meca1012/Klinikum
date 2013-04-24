9139b96bb8d10b8e766572e2ca07aeb1d608b4cf
Constantin Treiber <constantin.treiber@gmail.com>
2013-04-24 14:14:00 +0200

    Test

---
+++ Klinikum/src/test/MainTest.java
@@ -0,0 +1,92 @@
+package test;
+
+import org.openrdf.OpenRDFException;
+import org.openrdf.model.Literal;
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.vocabulary.RDF;
+import org.openrdf.query.BindingSet;
+import org.openrdf.query.QueryLanguage;
+import org.openrdf.query.TupleQuery;
+import org.openrdf.query.TupleQueryResult;
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+
+import sesameContext.Connection;
+
+public class MainTest {
+
+	/**
+	 * @param args
+	 * @throws RepositoryException 
+	 */
+	public static void main(String[] args) throws RepositoryException {
+		
+		
+		
+		Connection connectorfactory = new Connection();
+		Repository myRepo = connectorfactory.GetRepository();
+		RepositoryConnection repoCon = myRepo.getConnection();
+		
+		//Daten eintragen
+		System.out.println("THIS IS THE STATUS: " + repoCon.isOpen());
+		
+		ValueFactory f = myRepo.getValueFactory();
+		URI alice = f.createURI("http://LmuKlinikum.de/patient/alice");
+		URI bob = f.createURI("http://LmuKlinikum.de/patient/bob");
+		URI name = f.createURI("http://LmuKlinikum.de/ontology/name");
+		URI person = f.createURI("http://LmuKlinikum.de/ontology/person");
+		Literal bobsname = f.createLiteral("Bob");
+		Literal alicename = f.createLiteral("Alice");
+		
+		try {
+			
+			repoCon.add(alice, RDF.TYPE, person);
+			repoCon.add(alice, name, alicename);
+
+			repoCon.add(bob, RDF.TYPE, person);
+			repoCon.add(bob, name, bobsname);
+			
+				
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		
+		//Datenauslesen
+		try {
+			   
+			   try {
+			      String queryString = "SELECT * FROM {S} rdf:type {rdfs:Person}";
+			      TupleQuery tupleQuery = repoCon.prepareTupleQuery(QueryLanguage.SERQL, queryString);
+			      TupleQueryResult result = tupleQuery.evaluate();
+			      try {
+			    	  while (result.hasNext()) {
+			    		   BindingSet bindingSet = result.next();
+			    		   Value valueOfX = bindingSet.getValue("x");
+			    		   Value valueOfY = bindingSet.getValue("y");
+			    		   System.out.println(valueOfX.toString());
+			    		   System.out.println(valueOfY.toString());
+			    		   
+			    	  }
+			      }
+			      finally {
+			         result.close();
+			      }
+			   }
+			   finally {
+			      repoCon.close();
+			   }
+			}
+			catch (OpenRDFException e) {
+			   // handle exception
+			}
+		
+	
+	}
+
+}
