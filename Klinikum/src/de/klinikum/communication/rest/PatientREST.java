6acb9f765810882f10209fbea4ded498fcf4ad5e
meca1012 <carsten.meiser@googlemail.com>
2013-04-30 14:11:51 +0200

    ...

--- Klinikum/src/de/klinikum/communication/rest/PatientREST.java
+++ Klinikum/src/de/klinikum/communication/rest/PatientREST.java
@@ -2,24 +2,29 @@ package de.klinikum.communication.rest;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/patient")
-@Produces({ MediaType.APPLICATION_XML, MediaType.TEXT_XML, MediaType.APPLICATION_JSON })
+@Produces({ MediaType.APPLICATION_XML, MediaType.TEXT_XML,
+		MediaType.APPLICATION_JSON })
 @Consumes
 public class PatientREST {
 
-	@Path( "/test" )
-	public class MessageResource
-	{
-	  @GET
-	  @Produces( MediaType.TEXT_PLAIN )
-	  public String message()
-	  {
-	    return "Yea! ";
-	  }
+	@Path("/test")
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String test() {
+		return "Yea! ";
 	}
-	
+
+	@Path("/test2")
+	@POST
+	@Produces(MediaType.APPLICATION_XML)
+	public String newPatient() {
+		return "This should be a patient!";
+	}
+
 }
