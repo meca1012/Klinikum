0cdcb30a5809f99f02e12ec1ddf681a37699cce9
Constantin Treiber <constantin.treiber@gmail.com>
2013-05-02 11:05:16 +0200

    FolderStructure 	src/main 	src/test
    + JUnit in Pom
    - BasicTestClass

---
+++ KlinikumLMU/src/test/de/klinikum/test/basicTests.java
@@ -0,0 +1,14 @@
+package test.de.klinikum.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class basicTests {
+
+	@Test
+	public void test() {
+		assertEquals(true, true);
+	}
+
+}
