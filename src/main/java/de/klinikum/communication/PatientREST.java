08e063a35ab111c6f551d86da1627950377437a3
Constantin Treiber <constantin.treiber@gmail.com>
2013-05-04 16:15:46 +0200

    RestUser Impl

--- src/main/java/de/klinikum/communication/PatientREST.java
+++ src/main/java/de/klinikum/communication/PatientREST.java
@@ -4,8 +4,12 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import de.klinikum.domain.Patient;
+import de.klinikum.service.PatientService;
 
 @Path("/patient")
 @Produces({ MediaType.APPLICATION_XML, MediaType.TEXT_XML,
@@ -27,4 +31,20 @@ public class PatientREST {
 		return "This should be a patient!";
 	}
 
+	@Path("/getPatient/{patientNumber}")
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public Patient getPatient(@PathParam("patientNumber") String patientNumber) 
+	{
+		return PatientService.getPatient(patientNumber);
+	}
+	
+	@Path("/getPatient")
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public Patient getPatient() 
+	{
+		return PatientService.getPatient("123123123");
+	}
+	
 }
