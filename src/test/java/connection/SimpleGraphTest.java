d34f85f729a36c8e9bb72d266c38dfd60487eac4
Andreas Schillinger <andreas.schillinger@gmail.com>
2013-05-11 14:19:53 +0200

    Changed Simplegraph to use HTTP connection.

--- src/test/java/connection/SimpleGraphTest.java
+++ src/test/java/connection/SimpleGraphTest.java
@@ -1,5 +1,8 @@
 package connection;
 
+import java.util.List;
+
+import org.openrdf.model.Statement;
 import org.openrdf.model.URI;
 import org.openrdf.model.Value;
 
@@ -9,21 +12,26 @@ public class SimpleGraphTest {
 
     public static void main(String[] args) {
 	    // a test of graph operations
-	    SimpleGraph g = new SimpleGraph(true);
+	    SimpleGraph g = new SimpleGraph();
 	    
 	    // get LOD from a URI -  Jamie's FOAF profile from Hi5
 	    g.addURI("http://api.hi5.com/rest/profile/foaf/241087912");
 	    
 	    // manually add a triple/statement with a URIref object
-	    URI s1 = g.URIref("http://semprog.com/people/toby");
-	    URI p1 = g.URIref(SimpleGraph.RDFTYPE);
-	    URI o1 = g.URIref("http://xmlns.com/foaf/0.1/person");
-	    g.add(s1, p1, o1);
+	    URI bob = g.URIref("http://LmuKlinikum.de/patient/bob");
+	    URI predicate = g.URIref(SimpleGraph.RDFTYPE);
+	    URI person = g.URIref("http://LmuKlinikum.de/ontology/person");
+	    g.add(bob, predicate, person);	    
 	    
 	    // manually add with an object literal
-	    URI s2 = g.URIref("http://semprog.com/people/toby");
-	    URI p2 = g.URIref("http://xmlns.com/foaf/0.1/nick");
-	    Value o2 = g.Literal("kiwitobes");
-	    g.add(s2, p2, o2);	    
+	    URI name = g.URIref("http://LmuKlinikum.de/ontology/name");
+	    Value object = g.Literal("Bob");
+	    g.add(bob, name, object);
+	    
+	    List<Statement> result = g.tuplePattern(null, predicate, person);
+	    for(int i = 0; i < result.size(); i++) {
+	    	System.out.println("this is the result: " + result.get(i).getSubject().toString());
+	    	
+	    }
     }
 }
