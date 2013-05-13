57ab74e5455a8ca2ef3a2de5c395095a8d05b1e8
Andreas Schillinger <andreas.schillinger@gmail.com>
2013-05-13 09:56:11 +0200

    Commented Namespaces.

--- src/main/java/de/klinikum/domain/NameSpaces.java
+++ src/main/java/de/klinikum/domain/NameSpaces.java
@@ -3,12 +3,19 @@ package de.klinikum.domain;
 public enum NameSpaces {
 	
 	PATIENT_TYPE("http://spironto.de/spironto#patient"),
-	
+		
+	//Stammdaten
+	MASTER_DATA("http://spironto.de/spironto#is-masterData"),
 	PATIENT_NAME("http://spironto.de/spironto#has-name"),
+	//Hauptthema
+	MAIN_TOPIC("http://spironto.de/spironto#is-mainTopic"),
 	
-	DATE_ADDED("http://spironto.de/spironto#dateAdded"),
+	//vordefinierte Themen
 	
+	//Technisch
+	DATE_ADDED("http://spironto.de/spironto#dateAdded"),	
 	DATE_LAST_MODIFIED("http://spironto.de/spironto#dateLastModified");
+	
 
 	private final String uri;
 	
