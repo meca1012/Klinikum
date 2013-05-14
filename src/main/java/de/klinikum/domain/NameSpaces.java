4fbe246e58d974ea27a974a0ee58d2496bb071cc
Constantin Treiber <constantin.treiber@gmail.com>
2013-05-14 15:41:00 +0200

    Added some new NameSpaces and some Refactoring

--- src/main/java/de/klinikum/domain/NameSpaces.java
+++ src/main/java/de/klinikum/domain/NameSpaces.java
@@ -2,15 +2,59 @@ package de.klinikum.domain;
 
 public enum NameSpaces {
 	
-	PATIENT_TYPE("http://spironto.de/spironto#patient"),
-		
-	///Stammdaten
-	MASTER_DATA("http://spironto.de/spironto#is-masterData"),
-	PATIENT_NAME("http://spironto.de/spironto#has-name"),
-	///Hauptthema
-	MAIN_TOPIC("http://spironto.de/spironto#is-mainTopic"),
 	
-	///vordefinierte Themen
+	
+	// RDF Type for Patient
+	// ------------------------RDF PATIENT CORE DATA------------------------------
+	PATIENT_TYPE("http://spironto.de/spironto#patient"),
+	
+	// RDF Predicates for Patient
+	
+
+	// RDF Type for PATEINT CORE DATA
+	// Includes Street vNAME / nName  / Nr / Zip / City / Country
+	COREDATA_TYPE("http://spironto.de/spironto#patientCoredataType"),
+	COREDATA("http://spironto.de/spironto#patientCoredata"),
+	COREDATA_VNAME("http://spironto.de/spironto#has-vname"),
+	COREDATA_NNAME("http://spironto.de/spironto#has-nname"),
+	COREDATA_DAYOFBIRTH("http://spironto.de/spironto#is-born-on"),
+	COREDATA_ADDRESS("http://spironto.de/spironto#lives-in"),
+	COREDATA_ADRESS_STREET("http://spironto.de/spironto#address-street"),
+	COREDATA_ADRESS_ZIP("http://spironto.de/spironto#address-zip"),
+	COREDATA_ADRESS_CITY("http://spironto.de/spironto#address-city"),
+	COREDATA_ADRESS_Country("http://spironto.de/spironto#laddress-country"),	
+	COREDATA_PHONE("http://spironto.de/spironto#has-phonenumber"),
+	
+	// ------------------------END RDF PATIENT CORE DATA------------------------------
+	
+	
+	// ------------------------RDF THEMA CORE DATA------------------------------
+	// RDF Type for TextBlock 
+	// Includes Text , Date , Themes
+	TEXTBLOCK_TYPE("http://spironto.de/spironto#textBlock"),
+	TEXTBLOCK_TEXT("http://spironto.de/spironto#recorded-text"),
+	TEXTBLOCK_DATE("http://spironto.de/spironto#recorded-date"),
+	TEXTBLOCK_HEADTOPIC("http://spironto.de/spironto#head-topic"),
+	TEXTBLOCK_SUBTOPIC("http://spironto.de/spironto#head-topic"),
+		
+	// RDF Type for HeadTopic
+	// Used for SearchQuery like get everything linked to religion
+	HEADTOPIC("http://spironto.de/spironto#mainTopic"),
+
+	// RDF Type for SubTopyc 
+	// Used as Subtopic 
+	SUBTOPIC("http://spironto.de/spironto#subTopic"),
+
+	
+	// Predefined Topic
+	RELIGION_TYPE("http://spironto.de/spironto#head-topic"),
+	
+	// PalliativeTeam 
+	// Doctors, Nurses, Social worker
+	PTEAM_TYPE("http://sprironto.de//spironto#pTeam"),
+	
+	
+	
 	
 	///Technisch
 	//ID zur generierung von einzigartigen URI's
