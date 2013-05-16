3c5f44963271000d28fd2130c443fe657dca9b4e
meca1012 <carsten.meiser@googlemail.com>
2013-05-16 14:31:37 +0200

    added arquillian.xml pom.xml adjusted for arquillian and refactored version management PatientServiceTest for arquillian edited

--- src/test/java/service/PatientServiceTest.java
+++ src/test/java/service/PatientServiceTest.java
@@ -7,20 +7,37 @@ import java.io.IOException;
 
 import javax.inject.Inject;
 
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openrdf.repository.RepositoryException;
 
 import de.klinikum.domain.Address;
 import de.klinikum.domain.Patient;
+import de.klinikum.service.PatientService;
 import de.klinikum.service.PatientServiceImpl;
 
+@RunWith(Arquillian.class)
 public class PatientServiceTest {
 	
 	@Inject
 	PatientServiceImpl patientService;
 	
 	String patientUri;
-		
+	
+	/**
+	 */
+	@Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+            .addClass(PatientService.class)
+            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+	
 	@Test
 	public void testCreatePatient() throws IOException {
 		
