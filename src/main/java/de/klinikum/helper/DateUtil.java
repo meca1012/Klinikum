16c869ecaabc00d10956ddd0e451c36b8cc2b39b
Constantin Treiber <constantin.treiber@gmail.com>
2013-06-06 11:24:07 +0200

    CodeStyle

--- src/main/java/de/klinikum/helper/DateUtil.java
+++ src/main/java/de/klinikum/helper/DateUtil.java
@@ -8,19 +8,19 @@ import java.util.TimeZone;
 
 public class DateUtil {
 
-	 public static Date getBirthDateFromString(String sDate) {
-	        String dateFormat = "EEE MMM dd HH:mm:ss z yyyy";
+    public static Date getBirthDateFromString(String sDate) {
+        String dateFormat = "EEE MMM dd HH:mm:ss z yyyy";
 
-	        SimpleDateFormat sdf = new SimpleDateFormat(dateFormat, new Locale("en_US"));
-	        sdf.setTimeZone(TimeZone.getTimeZone("CET"));
-	        Date newDate = null;
-	        try {
-	            newDate = sdf.parse(sDate);
-	        }
-	        catch (ParseException e) {
-	            // TODO Auto-generated catch block
-	            e.printStackTrace();
-	        }
-	        return newDate;
-	    }
+        SimpleDateFormat sdf = new SimpleDateFormat(dateFormat, new Locale("en_US"));
+        sdf.setTimeZone(TimeZone.getTimeZone("CET"));
+        Date newDate = null;
+        try {
+            newDate = sdf.parse(sDate);
+        }
+        catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return newDate;
+    }
 }
