020df1a2d4979d61312de5574795b40654c0b6ee
meca1012 <carsten.meiser@googlemail.com>
2013-06-27 15:39:18 +0200

    Refactored Namespace

--- src/main/java/de/klinikum/persistence/NameSpaces.java
+++ src/main/java/de/klinikum/persistence/NameSpaces.java
@@ -10,68 +10,58 @@ package de.klinikum.persistence;
 
 public enum NameSpaces {
 
-    // RDF Type for Patient
-    // ------------------------RDF PATIENT + ADRESS_TYPE------------------------------
+    /*
+     * RDF-Types
+     */
     PERSON_TYPE("http://spironto.de/spironto#person"),
     PATIENT_TYPE("http://spironto.de/spironto#patient"),
     ADDRESS_TYPE("http://spironto.de/spironto#address"),
+    NOTE_TYPE("http://spironto.de/spironto#note"),
+    CONCEPT_TYPE("http://spironto.de/spironto#concept"),
+    GUI_TAB_TYPE("http://spironto.de/spironto#gui-tab-type"),
 
-    // RDF Predicates for Patient
-
-    // RDF Type for PATEINT CORE DATA
-    // Includes Street vNAME / nName / Nr / Zip / City / Country
+    /*
+     * RDF-Predicates for patient
+     */
     PATIENT_HAS_PATIENT_NUMBER("http://spironto.de/spironto#has-patient-number"),
     PATIENT_HAS_FIRST_NAME("http://spironto.de/spironto#has-first-name"),
     PATIENT_HAS_LAST_NAME("http://spironto.de/spironto#has-last-name"),
     PATIENT_HAS_DATE_OF_BIRTH("http://spironto.de/spironto#has-date-of-birth"),
     PATIENT_HAS_ADDRESS("http://spironto.de/spironto#has-address"),
     PATIENT_HAS_NOTE("http://spironto.de/spironto#has-note"),
+    PATIENT_HAS_CONCEPT("http://spironto.de/spironto#patient-has-concept"),
     PATIENT_POINTS_TO_CONCEPT("http://spironto.de/spironto#patient-points-to-concept"),
+    
+    /*
+     * RDF-Predicates for address
+     */
     ADDRESS_HAS_ADDRESS_STREET("http://spironto.de/spironto#has-address-street"),
     ADDRESS_HAS_ADDRESS_STREET_NUMBER("http://spironto.de/spironto#has-address-street-number"),
     ADDRESS_HAS_ADDRESS_ZIP("http://spironto.de/spironto#has-address-zip"),
     ADDRESS_HAS_ADDRESS_CITY("http://spironto.de/spironto#has-address-city"),
     ADDRESS_IN_COUNTRY("http://spironto.de/spironto#has-address-country"),
     ADDRESS_HAS_PHONENUMBER("http://spironto.de/spironto#has-phonenumber"),
-
-    // ------------------------END RDF PATIENT CORE DATA------------------------------
-
-    // ------------------------RDF THEMA CORE DATA------------------------------
-    // RDF Type for Note
-    // Includes Text , Date , Themes
-    NOTE_TYPE("http://spironto.de/spironto#note"),
+    
+    /*
+     * RDF-Predicates for note
+     */
     NOTE_HAS_TEXT("http://spironto.de/spironto#note-has-text"),
     NOTE_HAS_DATE("http://spironto.de/spironto#note-has-date"),
     NOTE_HAS_TITLE("http://spironto.de/spironto#note-has-title"),
     NOTE_HAS_PRIORITY("http://spironto.de/spironto#note-has-priority"),
     NOTE_POINTS_TO_CONCEPT("http://spironto.de/spironto#note-points-to-concept"),
 
-    // RDF Type for HeadTopic
-    // Used for SearchQuery like get everything linked to religion
-    ONTOLOGIE_CONCEPT_TYPE("http://spironto.de/spironto#concept"),
-    ONTOLOGIE_CONCEPT_HAS_LABEL("http://spironto.de/spironto#concept-has-label"),
-    ONTOLOGIE_CONCEPT_LINKED_TO("http://spironto.de/spironto#concept-linked-to"),
-    LINK_HAS_LABEL("http://spironto.de/spironto#link-has-label"),
+    /*
+     * RDF-Predicates for concept
+     * Used for SearchQuery like get everything linked to religion
+     */
+    CONCEPT_HAS_LABEL("http://spironto.de/spironto#concept-has-label"),
+    CONCEPT_LINKED_TO("http://spironto.de/spironto#concept-linked-to"),
     CONCEPT_IS_EDITABLE("http://spironto.de/spironto#concept-is-editable"),
 
-    // RDF Type for Ontology
-    PATIENT_HAS_CONCEPT("http://spironto.de/spironto#patient-has-concept"),
-
-    // Defined a Tap-Element in the GUI
-    // Predefined Topic
-    GUI_TAB_TYPE("http://spironto.de/spironto#gui-tab-type"),
-    GUI_TAB_TYPE_HAS_VALUE("http://spironto.de/spironto#gui-tab-type-has-value"),
-
-    // GEDANKEN MACHEN
-    // Predefined Topic
-    RELIGION_TYPE("http://spironto.de/spironto#head-topic"),
-
-    // GEDANKEN MACHEN
-    // PalliativeTeam
-    // Doctors, Nurses, Social worker
-    PTEAM_TYPE("http://sprironto.de//spironto#pTeam"),
-
-    // /Technical
+    /*
+     * Technical
+     */
     // ID for the generation of unique URIs
     LAST_ID("http://spironto.de/spironto#lastID"),
     TYPE_DATASTORE("http://spironto.de/spironto#Datastore"),
@@ -88,4 +78,5 @@ public enum NameSpaces {
     public String toString() {
         return this.uri;
     }
+    
 }
